3ab12b2712866210f3e29a049427e07c514af3d7
JatinShimpi <shimpijatin184@gmail.com>
2025-01-12 23:57:42 +0530

    added environment variables

--- src/main/java/com/jatin/ems_backend/MongoConfig.java
+++ src/main/java/com/jatin/ems_backend/MongoConfig.java
@@ -1,5 +1,7 @@
 package com.jatin.ems_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -9,9 +11,12 @@ import com.mongodb.client.MongoClients;
 @Configuration
 public class MongoConfig {
 
+    @Autowired
+    private Dotenv dotenv;
+
     @Bean
     public MongoTemplate mongoTemplate() {
-        MongoClient mongoClient = MongoClients.create("mongodb+srv://jls943754:zVkOvc1KkiG9ejvY@cluster0.xtgoy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");  // Mongo URI
+        MongoClient mongoClient = MongoClients.create(dotenv.get("MONGODB"));  // Mongo URI
         return new MongoTemplate(mongoClient, "your-database-name");  // Replace with your database name
     }
 }
